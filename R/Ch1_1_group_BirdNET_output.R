828ba4fbc39daa90cc287e60fa7307cda8d1d79a
Sunny Tseng <sunnyyctseng@gmail.com>
2024-07-19 16:47:34 -0700

    adjusted the code for combining the BirdNET output

--- R/Ch1_1_group_BirdNET_output.R
+++ R/Ch1_1_group_BirdNET_output.R
@@ -3,7 +3,7 @@
 ### 
 ### Author: Sunny Tseng
 ### Date: 2022-10-18
-### Input: year, season, and data_folder
+### Input: path
 ### Output: a tidy dataframe with BirdNET output from single season
 ###
 
@@ -15,36 +15,36 @@ library(here)
 library(janitor)
 
 
-
 # Function ----------------------------------------------------------------
 
-group_BirdNET_output <- function(year, season, data_folder){
+group_BirdNET_output <- function(path){
   
-  # list the files
-  year_season = paste0(year, "_", season)
-  file_list <- list.files(here(data_folder, year_season, "14_02"), pattern = ".txt$", recursive = TRUE)
-  
-  # combine all the txt files
-  data_full <- tibble()
-  for(file in file_list){
-    data <- read_delim(file = here(data_folder, year_season, "14_02", file), 
-                       delim = ";",
-                       col_type = list(`Start (s)` = col_double(),
-                                       `End (s)` = col_double(),
-                                       `Scientific name` = col_character(),
-                                       `Common name` = col_character(),
-                                       `Confidence` = col_double())) %>%
-      mutate(recording = file)
-    data_full <- bind_rows(data_full, data)
-  }
+  # combine all the output tables
+  data_full <- list.files(path,
+                          pattern = ".csv$", recursive = TRUE,
+                          full.names = TRUE) %>%
+    map_df(~ read_csv(file = .,
+                      col_types = cols(filepath = "c",
+                                       start = "d",
+                                       end = "d",
+                                       scientific_name = "c",
+                                       common_name = "c",
+                                       confidence = "d",
+                                       lat = "d",
+                                       lon = "d",
+                                       week = "d",
+                                       overlap = "d",
+                                       sensitivity = "d",
+                                       min_conf = "d",
+                                       species_list = "c",
+                                       model = "c")))
   
   # wrangle the big dataset
   data_full_format <- data_full %>%
     clean_names() %>%
-    separate(col = recording, into = c("site", "recording"), sep = "/") %>%
-    separate(col = recording, into = c("recording", NA), sep = ".txt") %>%
-    mutate(year = year, season = season, month = str_sub(recording, 5, 6), day = str_sub(recording, 7, 8)) %>%
-    select(year, month, day, season, site, recording, start_s, end_s, scientific_name, common_name, confidence)
+    mutate(site = str_split_i(filepath, pattern = "\\\\", i = 5),
+           recording = str_split_i(filepath, pattern = "\\\\", i = 6)) %>%
+    mutate(date = str_split_i(recording, pattern = ".WAV", i = 1) %>% as_datetime())
 
 return(data_full_format)
 }
@@ -53,8 +53,8 @@ return(data_full_format)
 # Example usage -----------------------------------------------------------
 
 
-final <- group_BirdNET_output(year = 2022, season = "passerine", data_folder = "data/Audio_output")
-write_csv(final, here("data", "processed", "2022_passerine_BirdNET_updated.csv"))
+final <- group_BirdNET_output(path = here("data", "2020_passerine_BirdNET_V2.4_C0.5"))
+#write_csv(final, here("data", "processed", "2022_passerine_BirdNET_V2.4_C0.5.csv"))
 
 
 
