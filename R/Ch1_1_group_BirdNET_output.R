571e786037b99d73bd3cc714aeca71bd886f37cf
Sunny Tseng <sunnyyctseng@gmail.com>
2024-07-22 11:58:30 -0700

    updated the table combining code

--- R/Ch1_1_group_BirdNET_output.R
+++ R/Ch1_1_group_BirdNET_output.R
@@ -39,22 +39,19 @@ group_BirdNET_output <- function(path){
                                        species_list = "c",
                                        model = "c")))
   
-  # wrangle the big dataset
-  data_full_format <- data_full %>%
-    clean_names() %>%
-    mutate(site = str_split_i(filepath, pattern = "\\\\", i = 5),
-           recording = str_split_i(filepath, pattern = "\\\\", i = 6)) %>%
-    mutate(date = str_split_i(recording, pattern = ".WAV", i = 1) %>% as_datetime())
-
-return(data_full_format)
+return(data_full)
 }
 
 
 # Example usage -----------------------------------------------------------
 
 
-final <- group_BirdNET_output(path = here("data", "2020_passerine_BirdNET_V2.4_C0.5"))
-#write_csv(final, here("data", "processed", "2022_passerine_BirdNET_V2.4_C0.5.csv"))
+data_full <- group_BirdNET_output(path = "F:/PhD thesis/data/Audio_output/2020_passerine_BirdNET_V2.4_C0.1")
+write_csv(data_full, paste0("F:/PhD thesis/data/Audio_output_combined/", "2020_passerine_BirdNET_V2.4_C0.1.csv"))
 
+data_full <- group_BirdNET_output(path = "F:/PhD thesis/data/Audio_output/2021_passerine_BirdNET_V2.4_C0.1")
+write_csv(data_full, paste0("F:/PhD thesis/data/Audio_output_combined/", "2021_passerine_BirdNET_V2.4_C0.1.csv"))
 
+data_full <- group_BirdNET_output(path = "F:/PhD thesis/data/Audio_output/2022_passerine_BirdNET_V2.4_C0.1")
+write_csv(data_full, paste0("F:/PhD thesis/data/Audio_output_combined/", "2022_passerine_BirdNET_V2.4_C0.1.csv"))
 
